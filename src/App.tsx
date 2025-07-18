5676f106060b8d7cba2de55ba1a608dab4ae5c20
Airlangga Pradana <ranggavfxartwork@gmail.com>
2025-07-18 15:00:12 +0700

    initialize React app with TypeScript, Vite, and Tailwind CSS

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <main className="w-full h-screen flex items-center text-3xl justify-center font-bold text-indigo-500">
+      Hello World!
+    </main>
+  )
+}
+
+export default App
